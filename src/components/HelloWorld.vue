6c67278f89c5c0f423b5de1e882a64e47a238caf
Aamir Khan <aamirkhan@beezlabs.com>
2021-03-16 16:01:12 +0530

    Checkpoint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,47 @@
 <template>
-    <div ref="childElement" style="width: 500px; height: 500px; margin: 0 auto;"></div>
+    <div>
+        <div
+            id="step1tour"
+            ref="childElement"
+            style="width: 500px; height: 500px; margin: 0 auto;"
+        ></div>
+        <v-tour name="tour" :steps="steps" />
+    </div>
 </template>
 
 <script>
-    import anime1 from '../../public/images/anime1.json'
-    import lottie from 'lottie-web'
+import anime1 from "../../public/images/anime1.json";
+import lottie from "lottie-web";
 
-    export default {
-        name: "HelloWorld",
+export default {
+    name: "HelloWorld",
 
-        data() {
-            return {
-                file: anime1
-            }
-        },
-
-        mounted() {
-            const element1 = this.$refs.childElement
-            console.log('jkj', this.$refs.childElement)
-            lottie.loadAnimation(
+    data() {
+        return {
+            file: anime1,
+            steps: [
                 {
-                    container: element1,
-                    renderer: 'svg',
-                    loop: true,
-                    autoplay: true,
-                    animationData: this.file
+                    target: 'step1tour',
+                    content: 'testing'
                 }
-            )
-        }
+            ]
+        };
+    },
 
-    };
+    mounted() {
+        const element1 = this.$refs.childElement;
+        console.log("jkj", this.$refs.childElement);
+        lottie.loadAnimation({
+            container: element1,
+            renderer: "svg",
+            loop: true,
+            autoplay: true,
+            animationData: this.file
+        });
+        this.$tours['tour'].start()
+    }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import VueTour from 'vue-tour'
+
+require('vue-tour/dist/vue-tour.css')
+
+Vue.use(VueTour)
 
 Vue.config.productionTip = false;
 
