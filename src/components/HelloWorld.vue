9c0103618eccbf72ef4131aaead1f7571700648d
Aamir Khan <aamirkhan@beezlabs.com>
2021-03-17 11:45:40 +0530

    Checkpoint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <template>
-    <div>
-        <div
-            id="step1tour"
-            ref="childElement"
-            style="width: 500px; height: 500px; margin: 0 auto;"
-        ></div>
-        <v-tour name="tour" :steps="steps" />
-    </div>
+  <div>
+    <div
+      id="step1tour"
+      ref="childElement"
+      style="width: 500px; height: 500px; margin: 0 auto;"
+    ></div>
+    <v-tour name="tour" :steps="steps" />
+  </div>
 </template>
 
 <script>
@@ -14,32 +14,32 @@ import anime1 from "../../public/images/anime1.json";
 import lottie from "lottie-web";
 
 export default {
-    name: "HelloWorld",
+  name: "HelloWorld",
 
-    data() {
-        return {
-            file: anime1,
-            steps: [
-                {
-                    target: 'step1tour',
-                    content: 'testing'
-                }
-            ]
-        };
-    },
+  data() {
+    return {
+      file: anime1,
+      steps: [
+        {
+          target: "step1tour",
+          content: "testing"
+        }
+      ]
+    };
+  },
 
-    mounted() {
-        const element1 = this.$refs.childElement;
-        console.log("jkj", this.$refs.childElement);
-        lottie.loadAnimation({
-            container: element1,
-            renderer: "svg",
-            loop: true,
-            autoplay: true,
-            animationData: this.file
-        });
-        this.$tours['tour'].start()
-    }
+  mounted() {
+    const element1 = this.$refs.childElement;
+    console.log("jkj", this.$refs.childElement);
+    lottie.loadAnimation({
+      container: element1,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: this.file
+    });
+    this.$tours["tour"].start();
+  }
 };
 </script>
 
